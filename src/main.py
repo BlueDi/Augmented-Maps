ce5e7abd6663046c2acdf1f3491de0109e1c7508
Diogo Cruz <diogohmcruz@hotmail.com>
2018-12-15 15:46:30 +0000

    Best match from db
    Closes #1

--- src/main.py
+++ src/main.py
@@ -1,27 +1,35 @@
 import cv2 as cv
+import os, os.path
 from display import *
 from fast import *
 from image import *
 from kaze import *
 
 
-def main():
-    #Open image
-    image_name1 = '../resources/porto_mapa.png'
-    image_name2 = '../resources/porto_mapa2.png'
-    image1 = open_image(image_name1)
-    image2 = open_image(image_name2)
-    cv.imshow('image', image1)
+IMAGE_FOLDER = '../resources/db'
+IMAGE_TEST = '../resources/test/clerigos.png'
 
-    #1. Extracting local features
-    fast_get_features(image1)
-    kaze_get_features(image1)
 
-    #2. Matching local features
-    image1, kaze_kp1, image2, kaze_kp2, filtered_matches, homography = kaze_match_results(image1, image2)
-    show_match_result(image1, kaze_kp1, image2, kaze_kp2, filtered_matches, homography)
-
+def search_all():
+    """
+    Search in the db for the best match
+    """
+    best_img = 0
+    best_matches = 0
+    image_test = open_image(IMAGE_TEST)
+    for f in os.listdir(IMAGE_FOLDER):
+        img_path = os.path.join(IMAGE_FOLDER,f)
+        img = open_image(img_path)
+        _, _, _, _, filtered_matches, _ = kaze_match_results(img, image_test)
+        if len(filtered_matches) > best_matches:
+            best_img = img
+            best_matches = len(filtered_matches)
+    cv.imshow('best', best_img)
     cv.waitKey(0)
+
+
+def main():    
+    search_all()
 
 
 if __name__ == '__main__':
