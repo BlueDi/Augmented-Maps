aef1b905962f304f79764221f244935ebfe0680a
Diogo Cruz <diogohmcruz@hotmail.com>
2018-12-16 20:19:44 +0000

    Display transformation using homography

--- src/display.py
+++ src/display.py
@@ -14,5 +14,12 @@ def show_match_result(imgA, keypointsA, imgB, keypointsB, matches, homography=No
 
     cv.namedWindow("matches", cv.WINDOW_KEEPRATIO)
     cv.imshow("matches", imgMatch)
+    transform_with_homography(imgB, homography)
     cv.waitKey(0)
+
+
+def transform_with_homography(image, homography):
+    dsize = (image.shape[1], image.shape[0])
+    new_image = cv.warpPerspective(image, homography, dsize)
+    cv.imshow("Homographied image", new_image)
 
